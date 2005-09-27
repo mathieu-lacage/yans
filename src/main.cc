6151c1eabd820c445cc0294cbcebca6cf95d2882
mlacage <mlacage@localhost.localdomain>
2005-09-27 13:56:28 +0100

    one test. one bug

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,11 @@
 /* -*-	Mode:C++; c-basic-offset:8; tab-width:8; indent-tabs-mode:t -*- */
 
+#include "test.h"
+
 int main (int argc, char *argv[])
 {
+	TestManager *manager = new TestManager ();
+	manager->enable_verbose ();
+	manager->run_tests ();
 	return 0;
 }
