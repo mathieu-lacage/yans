81191cee695ee2d19bbd66601b410fc3dad5f28f
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2005-10-03 12:48:51 +0200

    improve test script

--- test/test.pl
+++ test/test.pl
@@ -7,19 +7,24 @@ for ($size = 2; $size < 200; $size+= 2) {
     system ("make clean >compil.out 2>compil.err && make >>compil.out 2>>compil.err");
     system ("./test > /dev/null");
     $output = system ("./test > output");
-    open (OUTPUT, "output");
-    my $indirect;
-    my $direct;
-    my $sizeof;
-    while ($line = <OUTPUT>) {
-	if ($line =~ /indirect: ([0-9]+)ms/) {
-	    $indirect = $1;
-	} elsif ($line =~ /direct: ([0-9]+)ms/) {
-	    $direct = $1;
-	} elsif ($line =~ /sizeof: ([0-9]+)/) {
-	    $sizeof = $1;
+    my $indirect = 0;
+    my $direct = 0;
+    my $sizeof = 0;
+    for (my $i = 0; $i < 30; $i++) {
+	open (OUTPUT, "output");
+	while ($line = <OUTPUT>) {
+	    if ($line =~ /indirect: ([0-9]+)ms/) {
+		$indirect += $1;
+	    } elsif ($line =~ /direct: ([0-9]+)ms/) {
+		$direct += $1;
+	    } elsif ($line =~ /sizeof: ([0-9]+)/) {
+		$sizeof += $1;
+	    }
 	}
     }
+    $indirect /= 30;
+    $direct /= 30;
+    $sizeof /= 30;
     print "$size $sizeof $indirect $direct\n";
 }
 
