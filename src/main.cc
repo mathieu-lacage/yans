9b386a03ceca6be785597ac91f993131bb79ed60
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2005-10-06 16:25:45 +0200

    make sure test definitions are not here when not RUN_SELF_TESTS

--- src/main.cc
+++ src/main.cc
@@ -2,10 +2,14 @@
 
 #include "test.h"
 
+
 int main (int argc, char *argv[])
 {
+#ifdef RUN_SELF_TESTS
 	TestManager *manager = new TestManager ();
 	manager->enable_verbose ();
 	manager->run_tests ();
+#endif /* RUN_SELF_TESTS */
+	
 	return 0;
 }
