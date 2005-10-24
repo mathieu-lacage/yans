cee978449e07494ee5dae09a5c7202853527752b
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2005-10-24 16:58:58 +0200

    move other stuff around

---
+++ src/host/transport-protocol.cc
@@ -0,0 +1,6 @@
+/* -*-	Mode:C++; c-basic-offset:8; tab-width:8; indent-tabs-mode:t -*- */
+
+#include "transport-protocol.h"
+
+TransportProtocol::~TransportProtocol ()
+{}
---
+++ src/host/transport-protocol.h
@@ -0,0 +1,18 @@
+/* -*-	Mode:C++; c-basic-offset:8; tab-width:8; indent-tabs-mode:t -*- */
+
+#ifndef TRANSPORT_PROTOCOL_H
+#define TRANSPORT_PROTOCOL_H
+
+#include <stdint.h>
+
+class Packet;
+
+class TransportProtocol {
+public:
+	virtual ~TransportProtocol ();
+
+	virtual uint8_t get_protocol (void) = 0;
+	virtual void receive (Packet *packet) = 0;
+};
+
+#endif /* TRANSPORT_PROTOCOL_H */
