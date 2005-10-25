ee08158f822fea9ddee9dee4c720e4f14f4275ab
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2005-10-25 14:24:42 +0200

    add trace methods

--- src/host/tracer.h
+++ src/host/tracer.h
@@ -12,9 +12,16 @@ public:
 	void trace_tx_app  (Packet *packet);
 	void trace_tx_udp  (Packet *packet);
 	void trace_tx_ipv4 (Packet *packet);
+	void trace_tx_arp (Packet *packet);
+	void trace_tx_mac (Packet *packet);
+	void trace_tx_phy (Packet *packet);
+
 	void trace_rx_app  (Packet *packet);
 	void trace_rx_udp  (Packet *packet);
 	void trace_rx_ipv4 (Packet *packet);
+	void trace_rx_arp (Packet *packet);
+	void trace_rx_mac (Packet *packet);
+	void trace_rx_phy (Packet *packet);
 
 	void enable_all (void);
 	void disable_all (void);
