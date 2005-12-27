cac10dd061b3734ff2fd2e98de4f0111867298b3
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2005-12-27 17:00:45 +0200

    remove Read/Write Buffers

--- src/common/buffer.cc
+++ src/common/buffer.cc
@@ -265,165 +265,6 @@ Buffer::alloc_and_zero (uint32_t size)
 
 
 
-WriteBuffer::WriteBuffer (uint32_t size)
-{
-	m_write = 0;
-	if (size == 0) {
-		m_size = 16;
-	} else {
-		m_size = size;
-	}
-	m_buffer = new uint8_t [m_size];
-}
-
-WriteBuffer::~WriteBuffer ()
-{
-	delete [] m_buffer;
-}
-
-uint8_t *
-WriteBuffer::peek_data (void)
-{
-	return m_buffer;
-}
-uint32_t 
-WriteBuffer::get_written_size (void)
-{
-	return m_write;
-}
-
-void 
-WriteBuffer::reset (void)
-{
-	m_write = 0;
-}
-
-void
-WriteBuffer::ensure_write_room_left (uint16_t needed)
-{
-	if (m_size > (m_write + needed)) {
-		return;
-	}
-	uint32_t new_size = m_size;
-	assert (new_size > 0);
-	while (new_size < (m_write + needed)) {
-		new_size <<= 1;
-	}
-	uint8_t *new_data = new uint8_t [new_size];
-	memcpy (new_data, m_buffer, m_write);
-	delete [] m_buffer;
-	m_buffer = new_data;
-	m_size = new_size;
-}
-void 
-WriteBuffer::write_u8 (uint8_t data)
-{
-	ensure_write_room_left (1);
-	m_buffer[m_write] = data;
-	m_write++;
-}
-void 
-WriteBuffer::write (uint8_t const *data, uint16_t size)
-{
-	ensure_write_room_left (size);
-	memcpy (m_buffer+m_write, data, size);
-	m_write += size;
-}
-void 
-WriteBuffer::write_hton_u16 (uint16_t data)
-{
-	ensure_write_room_left (2);
-	m_buffer[m_write] = (data >> 8) & 0xff;
-	m_write++;
-	m_buffer[m_write] = (data >> 0) & 0xff;
-	m_write++;
-}
-
-void 
-WriteBuffer::write_hton_u32 (uint32_t data)
-{
-	ensure_write_room_left (4);
-	m_buffer[m_write] = (data >> 24)  & 0xff;
-	m_write++;
-	m_buffer[m_write] = (data >> 16)  & 0xff;
-	m_write++;
-	m_buffer[m_write] = (data >> 8) & 0xff;
-	m_write++;
-	m_buffer[m_write] = (data >> 0) & 0xff;
-	m_write++;
-}
-
-
-ReadBuffer::ReadBuffer (uint32_t size)
-{
-	m_size = size;
-	m_buffer = new uint8_t [m_size];
-}
-ReadBuffer::~ReadBuffer ()
-{
-	delete m_buffer;
-	m_buffer = 0;
-	m_size = 0;
-}
-uint8_t *
-ReadBuffer::peek_data (void)
-{
-	return m_buffer;
-}
-bool
-ReadBuffer::is_read_room_left (uint16_t needed)
-{
-	if ((m_size - m_read) >= needed) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-
-uint8_t 
-ReadBuffer::read_u8 (void)
-{
-	assert (is_read_room_left (1));
-	uint8_t retval = m_buffer[m_read];
-	m_read++;
-	return retval;
-}
-void 
-ReadBuffer::read (uint8_t *buffer, uint16_t size)
-{
-	assert (is_read_room_left (size));
-	memcpy (buffer, m_buffer+m_read, size);
-	m_read += size;
-}
-
-uint16_t 
-ReadBuffer::read_ntoh_u16 (void)
-{
-	assert (is_read_room_left (2));
-	uint16_t retval = 0;
-	retval |= m_buffer[m_read] << 8;
-	m_read++;
-	retval |= m_buffer[m_read] << 0;
-	m_read++;
-	return retval;
-}
-
-uint32_t 
-ReadBuffer::read_ntoh_u32 (void)
-{
-	assert (is_read_room_left (4));
-	uint32_t retval = 0;
-	retval |= m_buffer[m_read] << 24;
-	m_read++;
-	retval |= m_buffer[m_read] << 16;
-	m_read++;
-	retval |= m_buffer[m_read] << 8;
-	m_read++;
-	retval |= m_buffer[m_read] << 0;
-	m_read++;
-	return retval;
-}
 
 
 
@@ -437,12 +278,12 @@ BufferTest::BufferTest (TestManager *manager)
 {}
 
 bool
-BufferTest::ensure_written_bytes (WriteBuffer *buffer, uint32_t n, uint8_t array[])
+BufferTest::ensure_written_bytes (Buffer *buffer, uint32_t n, uint8_t array[])
 {
 	bool success = true;
 	uint8_t *expected = array;
 	uint8_t *got;
-	if (buffer->get_written_size () != n) {
+	if (buffer->get_current () != n) {
 		success = false;
 	}
 	got = buffer->peek_data ();
@@ -452,7 +293,7 @@ BufferTest::ensure_written_bytes (WriteBuffer *buffer, uint32_t n, uint8_t array
 		}
 	}
 	if (!success) {
-		failure () << "WriteBuffer -- ";
+		failure () << "Buffer -- ";
 		failure () << "expected: ";
 		failure () << n << " ";
 		failure ().setf (std::ios::hex, std::ios::basefield);
@@ -461,7 +302,7 @@ BufferTest::ensure_written_bytes (WriteBuffer *buffer, uint32_t n, uint8_t array
 		}
 		failure ().setf (std::ios::dec, std::ios::basefield);
 		failure () << "got: ";
-		failure () << buffer->get_written_size () << " ";
+		failure () << buffer->get_current () << " ";
 		failure ().setf (std::ios::hex, std::ios::basefield);
 		for (uint32_t i = 0; i < n; i++) {
 			failure () << (uint16_t)got[i] << " ";
@@ -487,15 +328,15 @@ bool
 BufferTest::run_tests (void)
 {
 	bool ok = true;
-	WriteBuffer *buffer = new WriteBuffer (1);
+	Buffer *buffer = new Buffer (1);
 	buffer->write_u8 (0x66);
 	ENSURE_WRITTEN_BYTES (buffer, 1, 0x66);
-	buffer->reset ();
+	buffer->seek (0);
 	buffer->write_u8 (0x67);
 	ENSURE_WRITTEN_BYTES (buffer, 1, 0x67);
 	buffer->write_hton_u16 (0x6568);
 	ENSURE_WRITTEN_BYTES (buffer, 3, 0x67, 0x68, 0x65);
-	buffer->reset ();
+	buffer->seek (0);
 	buffer->write_hton_u16 (0x6369);
 	ENSURE_WRITTEN_BYTES (buffer, 2, 0x69, 0x63);
 	buffer->write_hton_u32 (0xdeadbeaf);
--- src/common/buffer.h
+++ src/common/buffer.h
@@ -63,48 +63,6 @@ private:
 	uint8_t *m_buffer;
 };
 
-class WriteBuffer {
-public:
-	WriteBuffer (uint32_t prefered_size);
-	~WriteBuffer ();
-
-	/* used to copy the data out of this write buffer. */
-	uint8_t *peek_data (void);
-	uint32_t get_written_size (void);
-	void reset (void);
-
-	void write_u8 (uint8_t data);
-	void write (uint8_t const*buffer, uint16_t size);
-	void write_hton_u16 (uint16_t data);
-	void write_hton_u32 (uint32_t data);
-
-private:
-	void ensure_write_room_left (uint16_t needed);
-	uint32_t m_size;
-	uint32_t m_write;
-	uint8_t *m_buffer;
-};
-
-class ReadBuffer {
-public:
-	ReadBuffer (uint32_t size);
-	~ReadBuffer ();
-
-	/* used to copy the data in this read buffer. */
-	uint8_t *peek_data (void);
-
-	uint8_t read_u8 (void);
-	void read (uint8_t *buffer, uint16_t size);
-	uint16_t read_ntoh_u16 (void);
-	uint32_t read_ntoh_u32 (void);
-
-private:
-	bool is_read_room_left (uint16_t needed);
-	uint32_t m_size;
-	uint32_t m_read;
-	uint8_t *m_buffer;
-};
-
 #ifdef RUN_SELF_TESTS
 #include "test.h"
 class BufferTest: public Test {
@@ -113,7 +71,7 @@ public:
 	
 	virtual bool run_tests (void);
 private:
-	bool ensure_written_bytes (WriteBuffer *buffer, 
+	bool ensure_written_bytes (Buffer *buffer, 
 				   uint32_t n, uint8_t array[]);
 };
 #endif /* RUN_SELF_TESTS */
