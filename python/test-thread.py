431c144cbf806b0c1dd762516748e655ded51c06
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2006-01-04 12:15:18 +0200

    debugging for thread python module

--- python/test-thread.py
+++ python/test-thread.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
+import yans;
 from yans import *;
 
-class MyThread(Thread):
+class MyThread(yans.Thread):
+#class MyThread:
     def run(self):
-        print "runnning";
+        print "before sleep";
+#        sleep_s (1.0);
+#        print "after sleep %f" % simulator.now_s ();
+        print "after sleep %f" % time_s ();
 
-        
+#thread = MyThread ();
+#simulator.insert_at_s (1.0, simulator.make_event (MyThread.run, thread));
+thread = MyThread ("test");
 
 simulator.run ()
 
