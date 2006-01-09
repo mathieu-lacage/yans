318bf3e62a0e0940d07b533ba48c92cc4f81aa5f
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2006-01-09 14:35:26 +0200

    port udp

--- src/ipv4/udp.cc
+++ src/ipv4/udp.cc
@@ -88,12 +88,12 @@ Udp::receive (Packet *packet)
 	m_host->get_tracer ()->trace_rx_udp (packet);
 	TagInIpv4 *tag = static_cast <TagInIpv4 *> (packet->get_tag (TagInIpv4::get_tag ()));
 	assert (tag != 0);
-	ChunkUdp *udp_chunk = static_cast <ChunkUdp *> (packet->remove_header ());
-	tag->set_dport (udp_chunk->get_destination ());
-	tag->set_sport (udp_chunk->get_source ());
+	ChunkUdp udp_chunk;
+	packet->remove (&udp_chunk);
+	tag->set_dport (udp_chunk.get_destination ());
+	tag->set_sport (udp_chunk.get_source ());
 	Ipv4EndPoint *end_point = m_end_points->lookup (tag->get_daddress (), tag->get_dport (),
 							tag->get_saddress (), tag->get_sport ());
-	delete udp_chunk;
 	if (end_point == 0) {
 		return;
 	}
@@ -105,17 +105,16 @@ Udp::send (Packet *packet)
 {
 	TagOutIpv4 *tag = static_cast <TagOutIpv4 *> (packet->get_tag (TagOutIpv4::get_tag ()));
 	assert (tag != 0);
-	ChunkUdp *udp_chunk = new ChunkUdp ();
-	udp_chunk->set_destination (tag->get_dport ());
-	udp_chunk->set_source (tag->get_sport ());
-	udp_chunk->set_payload_size (packet->get_size ());
+	ChunkUdp udp_chunk;
+	udp_chunk.set_destination (tag->get_dport ());
+	udp_chunk.set_source (tag->get_sport ());
+	udp_chunk.set_payload_size (packet->get_size ());
 
-	packet->add_header (udp_chunk);
+	packet->add (&udp_chunk);
 
 	m_ipv4->set_protocol (UDP_PROTOCOL);
 	m_host->get_tracer ()->trace_tx_udp (packet);
 	m_ipv4->send (packet);
-	// XXX: if we wanted to, we could generate the udp checksum here.
 }
 
 
