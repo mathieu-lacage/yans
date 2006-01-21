0d4c48a4a476f4ed063ed9bd450ceaf835217e04
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2006-01-21 13:37:43 +0200

    thread c++ test

---
+++ src/samples/main-thread.cc
@@ -0,0 +1,84 @@
+#include "thread.h"
+#include "semaphore.h"
+#include "simulator.h"
+
+#include <iostream>
+# define TRACE(x) \
+  std::cout << "TEST THREAD " << yans::Simulator::now_s () << " " << x << std::endl;
+
+
+class A : public yans::Thread {
+public:
+  A () : yans::Thread ("A") {}
+private:
+  virtual void run (void) {
+    TRACE ("A run");
+    sleep_s (1.0);
+    TRACE ("A run completed");
+  }
+};
+
+class B : public yans::Thread {
+public:
+  B () : yans::Thread ("B"), 
+	 m_sem (new yans::Semaphore (0)),
+	 m_osem (new yans::Semaphore (0))
+  {}
+  ~B () {delete m_sem; delete m_osem;}
+
+  void wait_until_notify (void) {
+    m_sem->down ();
+    m_osem->up ();
+  }
+private:
+  virtual void run (void) {
+    TRACE ("B run");
+    sleep_s (10.0);
+    m_sem->up ();
+    m_osem->down ();
+    TRACE ("B run completed");
+  }
+  yans::Semaphore *m_sem;
+  yans::Semaphore *m_osem;
+};
+
+class C : public yans::Thread {
+public:
+  C (B *b) : yans::Thread ("C"), m_b (b) {}
+private:
+  virtual void run (void) {
+    TRACE ("C run");
+    m_b->wait_until_notify ();
+    TRACE ("C completed");
+  }
+  B* m_b;
+};
+
+
+int main (int argc, char *argv[])
+{
+  yans::Thread *a = new A ();
+
+  TRACE ("a created");
+
+  yans::Simulator::run ();
+
+  TRACE ("simulation completed");
+
+  delete a;
+
+  yans::Simulator::destroy ();
+
+
+  B *b = new B ();
+  C *c = new C (b);
+
+  yans::Simulator::run ();
+
+  yans::Simulator::destroy ();
+
+  delete b;
+  delete c;
+
+  return 0;
+}
