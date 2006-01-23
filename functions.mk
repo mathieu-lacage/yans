3b24bc477e8c7f4fc03dc6268ca28d8ba418c460
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2006-01-23 16:41:41 +0200

    forgot to build .s files

--- functions.mk
+++ functions.mk
@@ -5,6 +5,7 @@ NULL=
 gen-bin=$(addprefix $(TOP_BUILD_DIR)/, $1)
 gen-obj= $(call gen-bin, \
 	$(addsuffix .o, $(basename $(filter %.c,$1))) \
+	$(addsuffix .o, $(basename $(filter %.s,$1))) \
 	$(addsuffix .o, $(basename $(filter %.cc,$1))) \
 	$(filter %.py,$1) \
 )
