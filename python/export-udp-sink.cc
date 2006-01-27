e4d60b565cbfd0950bc6c27f70aae16c7d55aad0
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2006-01-27 18:16:55 +0200

    add sink methods

--- python/export-udp-sink.cc
+++ python/export-udp-sink.cc
@@ -2,12 +2,30 @@
 
 #include <boost/python.hpp>
 #include "udp-sink.h"
+#include "function-holder.h"
+#include "export-callback.tcc"
 #include "host.h"
 
 using namespace boost::python;
 using namespace yans;
 
+static void set_receive_callback_cpp (UdpSink *self, std::auto_ptr<CallbackBase> callback)
+{
+	self->set_receive_callback (static_cast<Callback<void (Packet *)> *> (callback.get ()));
+	callback.release ();
+}
+static void set_receive_callback_python (UdpSink *self, FunctionHolder holder)
+{
+	self->set_receive_callback (new ExportCallback1<void, Packet *> (holder));
+}
+
+
+
 void export_udp_sink (void)
 {
-	class_<UdpSink, boost::noncopyable> ("UdpSink", init<Host *> ());
+	class_<UdpSink, boost::noncopyable> sink ("UdpSink", init<Host *> ());
+	sink.def ("bind", &UdpSink::bind);
+	sink.def ("unbind_at", &UdpSink::unbind_at);
+	sink.def ("set_receive_callback", &set_receive_callback_python);
+	sink.def ("set_receive_callback", &set_receive_callback_cpp);
 }
