c2d7da821c06dd31da93e37b8bde3629d999dde6
mathieu lacage <mathieu.lacage@sophia.inria.fr>
2006-02-06 08:57:01 +0100

    [project @ 8d1dd0473396e134d66113778a0561d3359a9336] add new phyber class.

---
+++ src/80211/phy-ber.h
@@ -0,0 +1,33 @@
+/* -*-	Mode:C++; c-basic-offset:8; tab-width:8; indent-tabs-mode:t -*- */
+/*
+ * Copyright (c) 2005 INRIA
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as 
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
+ */
+
+#ifndef PHY_BER_H
+#define PHY_BER_H
+
+#include "phy-80211.h"
+
+class PhyBer : public Phy80211 {
+public:
+	PhyBer ();
+	virtual ~PhyBer ();
+};
+
+#endif /* PHY_BER_H */
