c2fa370fc3a6bbe96b8e3ac04e5a9d3904832541
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2006-02-06 09:37:02 +0100

    add some documentation to the same scripts.

--- src/80211/test-80211.tcl
+++ src/80211/test-80211.tcl
@@ -15,6 +15,25 @@ $nodes(0) set-position 0.0 0.0 0.0
 $nodes(1) set-position 200.0 0.0 0.0
 $nodes(2) set-position 0.0 200.0 0.0
 
+# Set various PHY models
+# the default is ET. See the README for
+# a description of these models.
+#$interfaceConstructor set-phy-model-snrt
+#$interfaceConstructor set-phy-model-ber
+#$interfaceConstructor set-phy-model-et
+
+
+# set ARF rate control
+#$interfaceConstructor set-arf
+# set AARF rate control
+#$interfaceConstructor set-aarf
+# constant rate: 2 for data frames and 0 for control and management frames
+#$interfaceConstructor set-cr 2 0
+
+
+# for adhoc mode, use set-adhoc
+#$interfaceConstructor set-adhoc;
+
 $interfaceConstructor set-nqap;
 $nodes(0) add-interface [$interfaceConstructor create-interface] $channel
 $interfaceConstructor set-nqsta 1;
