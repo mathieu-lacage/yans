e271aec3989ce874abdbf206351bbdc5d753ed7e
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2006-02-17 10:16:36 +0100

    port MacStations

--- src/80211/mac-stations.cc
+++ src/80211/mac-stations.cc
@@ -1,6 +1,6 @@
 /* -*-	Mode:C++; c-basic-offset:8; tab-width:8; indent-tabs-mode:t -*- */
 /*
- * Copyright (c) 2005 INRIA
+ * Copyright (c) 2005,2006 INRIA
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -16,34 +16,12 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * In addition, as a special exception, the copyright holders of
- * this module give you permission to combine (via static or
- * dynamic linking) this module with free software programs or
- * libraries that are released under the GNU LGPL and with code
- * included in the standard release of ns-2 under the Apache 2.0
- * license or under otherwise-compatible licenses with advertising
- * requirements (or modified versions of such code, with unchanged
- * license).  You may copy and distribute such a system following the
- * terms of the GNU GPL for this module and the licenses of the
- * other code concerned, provided that you include the source code of
- * that other code when and as the GNU GPL requires distribution of
- * source code.
- *
- * Note that people who make modified versions of this module
- * are not obligated to grant this special exception for their
- * modified versions; it is their choice whether to do so.  The GNU
- * General Public License gives permission to release a modified
- * version without this exception; this exception also makes it
- * possible to release a modified version which carries forward this
- * exception.
- *
  * Author: Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
  */
 
 #include "mac-stations.h"
-#include "phy-80211.h"
-#include "net-interface-80211.h"
 
+namespace yans {
 
 MacStations::MacStations ()
 {}
@@ -51,24 +29,23 @@ MacStations::MacStations ()
 MacStations::~MacStations ()
 {}
 
-void
-MacStations::setInterface (NetInterface80211 *interface)
-{
-	m_interface = interface;
-}
-
 MacStation *
-MacStations::lookup (int address)
+MacStations::lookup (MacAddress address)
 {
-	if (m_addresses[address] == 0) {
-		m_addresses[address] = createStation ();
+	for (StationsI i = m_stations.begin (); i != m_stations.end (); i++) {
+		if ((*i).first.is_equal (address)) {
+			return (*i).second;
+		}
 	}
-	return m_addresses[address];
+	MacStation *station = createStation ();
+	m_stations.push_back (std::make_pair (address, station));
+	return station;
 }
 
 int 
-MacStations::getNModes (void)
+MacStations::get_n_modes (void) const
 {
-	return m_interface->phy ()->getNModes ();
+	return m_n_modes;
 }
 
+}; // namespace yans
