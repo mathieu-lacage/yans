de1acc0e3ed67d0fa1adf76df6bc9dbef2b62b76
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2006-02-20 15:48:45 +0100

    new config.mk file

---
+++ config.mk
@@ -0,0 +1,7 @@
+PYTHON_PREFIX_INC=/usr/include/python2.3
+PYTHON_PREFIX_LIB=/usr/lib
+PYTHON_BIN=/usr/bin/python2.3
+BOOST_PREFIX_LIB=/usr/lib
+BOOST_PREFIX_INC=/usr/include
+GSL_PREFIX_INC=/usr/include/gsl
+GSL_PREFIX_LIB=/usr/lib
