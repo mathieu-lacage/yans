fe0591af69dcd65a8847d0ecd6fd82a4a3fce02e
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2006-03-16 09:37:00 +0100

    remove licence exception

--- src/80211/mac-rx-middle.cc
+++ src/80211/mac-rx-middle.cc
@@ -16,27 +16,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * In addition, as a special exception, the copyright holders of
- * this module give you permission to combine (via static or
- * dynamic linking) this module with free software programs or
- * libraries that are released under the GNU LGPL and with code
- * included in the standard release of ns-2 under the Apache 2.0
- * license or under otherwise-compatible licenses with advertising
- * requirements (or modified versions of such code, with unchanged
- * license).  You may copy and distribute such a system following the
- * terms of the GNU GPL for this module and the licenses of the
- * other code concerned, provided that you include the source code of
- * that other code when and as the GNU GPL requires distribution of
- * source code.
- *
- * Note that people who make modified versions of this module
- * are not obligated to grant this special exception for their
- * modified versions; it is their choice whether to do so.  The GNU
- * General Public License gives permission to release a modified
- * version without this exception; this exception also makes it
- * possible to release a modified version which carries forward this
- * exception.
- *
  * Author: Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
  */
 
@@ -50,16 +29,15 @@
 
 #include "packet.h"
 
-#ifndef MAC_MIDDLE_TRACE
-#define nopeMAC_MIDDLE_TRACE 1
-#endif /* MAC_MIDDLE_TRACE */
+#define nopeRX_MIDDLE_TRACE 1
 
-#ifdef MAC_MIDDLE_TRACE
-# define TRACE(format, ...) \
-	printf ("MAC MIDDLE %d " format "\n", m_interface->getMacAddress (), ## __VA_ARGS__);
-#else /* MAC_TRACE */
-# define TRACE(format, ...)
-#endif /* MAC_TRACE */
+#ifdef RX_MIDDLE_TRACE
+#include <iostream>
+# define TRACE(x) \
+	printf ("RX MIDDLE "<<x<<std::endl);
+#else /* RX_MIDDLE_TRACE */
+# define TRACE(x)
+#endif /* RX_MIDDLE_TRACE */
 
 
 class OriginatorRxStatus {
