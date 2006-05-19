b0af08c0aae4bdd244a67bf693ee0e14e0c82d7a
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2006-05-19 15:55:17 +0200

    fix main-simple sample program

--- samples/main-simple.cc
+++ samples/main-simple.cc
@@ -57,27 +57,22 @@ int main (int argc, char *argv[])
 {
 	/* setup the ethernet network itself. */
 	EthernetNetworkInterface *eth_client, *eth_server;
-	eth_client = new EthernetNetworkInterface ("eth0");
-	eth_server = new EthernetNetworkInterface ("eth0");
-	eth_client->set_mac_address (MacAddress ("00:00:00:00:00:01"));
-	eth_server->set_mac_address (MacAddress ("00:00:00:00:00:02"));
+	eth_client = new EthernetNetworkInterface (MacAddress ("00:00:00:00:00:01"), "eth0");
+	eth_server = new EthernetNetworkInterface (MacAddress ("00:00:00:00:00:02"), "eth0");
 	Cable *cable = new Cable ();
 	cable->connect_to (eth_client, eth_server);
-
-	/* associate ipv4 addresses to the ethernet network elements */
-	eth_client->set_ipv4_address (Ipv4Address ("192.168.0.3"));
-	eth_client->set_ipv4_mask (Ipv4Mask ("255.255.255.0"));
-	eth_server->set_ipv4_address (Ipv4Address ("192.168.0.2"));
-	eth_server->set_ipv4_mask (Ipv4Mask ("255.255.255.0"));
-	eth_client->set_up ();
-	eth_server->set_up ();
 
 	/* create hosts for the network elements*/
 	Host *hclient, *hserver;
 	hclient = new Host ("client");
 	hserver = new Host ("server");
-	hclient->add_interface (eth_client);
-	hserver->add_interface (eth_server);
+	Ipv4NetworkInterface *ip_client, *ip_server;
+	ip_client = hclient->add_ipv4_arp_interface (eth_client, 
+						     Ipv4Address ("192.168.0.3"),
+						     Ipv4Mask ("255.255.255.0"));
+	ip_server = hserver->add_ipv4_arp_interface (eth_server,
+						     Ipv4Address ("192.168.0.2"),
+						     Ipv4Mask ("255.255.255.0"));
 
 	/* setup tracing for eth0-level interfaces in pcap files. */
 	PcapWriter *client_pcap = setup_pcap_trace (eth_client, "client-log-eth0");
@@ -85,9 +80,9 @@ int main (int argc, char *argv[])
 
 	/* setup the routing tables. */
 	hclient->get_routing_table ()->set_default_route (Ipv4Address ("192.168.0.2"),
-							  eth_client);
+							  ip_client);
 	hserver->get_routing_table ()->set_default_route (Ipv4Address ("192.168.0.3"),
-							  eth_server);
+							  ip_server);
 
 	/* create udp source endpoint. */
 	UdpSource *source = new UdpSource (hclient);
