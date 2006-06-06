b1af0331ecb06ddc7727ac93ae0a750d47d48fd0
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2006-06-06 17:09:19 +0200

    new idl interfaces for the corba parallel code, parallel 802.11 channel

---
+++ src/corba/parallel-channel-80211.cc
@@ -0,0 +1,125 @@
+/* -*-	Mode:C++; c-basic-offset:8; tab-width:8; indent-tabs-mode:t -*- */
+/*
+ * Copyright (c) 2006 INRIA
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as 
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Mathieu Lacage, <mathieu.lacage@sophia.inria.fr>
+ */
+#include "parallel-channel-80211.h"
+
+class Channel80211Queue : public yans::ParallelSimulatorQueue {
+public:
+	Channel80211Queue (ParallelChannel80211 *channel);
+	~Channel80211Queue ();
+private:
+	void send_null_message (void);
+	ParallelChannel80211 *m_channel;
+};
+
+Channel80211Queue::Channel80211Queue (ParallelChannel80211 *channel)
+	: m_channel (channel)
+{}
+
+Channel80211Queue::~Channel80211Queue ()
+{}
+
+void
+Channel80211Queue::send_null_message (void)
+{
+	m_channel->send_null_message ();
+}
+
+ParallelChannel80211::ParallelChannel80211 ()
+{
+	m_queue = new Channel80211Queue (this);
+	yans::Simulator::add_parallel_queue (m_queue);
+}
+ParallelChannel80211::~ParallelChannel80211 ()
+{
+	delete m_queue;
+}
+void 
+ParallelChannel80211::add (Remote::Channel80211_var channel)
+{
+	m_channels.push_back (channel);
+}
+void 
+ParallelChannel80211::receive (yans::Packet const *packet, double tx_power_dbm,
+			       double from_x, double from_y, double from_z,
+			       uint8_t tx_mode, uint8_t stuff)
+{
+	for (ModelsCI i = m_models.begin (); i != m_models.end (); i++) {
+		uint64_t delay_us = (*i)->get_delay_us (from_x, from_y, from_z);
+		double rx_power_w = (*i)->get_rx_power_w (tx_power_dbm, from_x, from_y, from_z);
+		m_queue->insert_at_us (delay_us, yans::make_event (&forward_up, 
+								   yans::make_count_ptr_holder (packet),
+								   rx_power_w, tx_mode, stuff, *i));
+	}
+}
+void 
+ParallelChannel80211::real_add (yans::PropagationModel *model)
+{
+	m_models.push_back (model);
+}
+void 
+ParallelChannel80211::real_send (yans::Packet const *packet, double tx_power_dbm,
+				 uint8_t tx_mode, uint8_t stuff, 
+				 yans::PropagationModel const*caller) const
+{
+	double x, y, z;
+	caller->get_position (x,y,z);
+	for (ModelsCI i = m_models.begin (); i != m_models.end (); i++) {
+		if (caller != (*i)) {
+			uint64_t delay_us = (*i)->get_delay_us (from_x, from_y, from_z);
+			double rx_power_w = (*i)->get_rx_power_w (tx_power_dbm, from_x, from_y, from_z);
+			yans::Simulator::insert_in_us (delay_us, yans::make_event (&forward_up, 
+										   yans::make_count_ptr_holder (packet),
+										   rx_power_w, tx_mode, stuff, *i));
+		}
+	}
+	struct Remote::SourcePosition position;
+	position.x = x;
+	position.y = y;
+	position.z = z;
+	uint8_t *data = packet->begin ().peek_data ();
+	Remote::Buffer_var buffer = Remote::Buffer (packet->get_size (),
+						    packet->get_size (),
+						    data, 
+						    0 /* make sure we (the caller) keep ownership
+						       of the data buffer. */);
+	
+	for (ChannelsI j = m_channels.begin (); j != m_channels.end (); j++) {
+		(*i)->receive (position, Simulator::now_us (), buffer,
+			       tx_power_dbm, tx_mode, stuff);
+	}
+}
+
+void
+ParallelChannel80211::send_null_message (void)
+{
+	Remote::SourcePositions_var sources;
+	Remote::SourcePosition position;
+	double x, y, z;
+	uint32_t index = 0;
+	for (ModelsCI i = m_models.begin (); i != m_models.end (); i++) {
+		(*i)->get_position (position.x, position.y, position.z);
+		sources[index] = position;
+		index++;
+	}
+	for (ChannelsI j = m_channels.begin (); j != m_channels.end (); j++) {
+		(*i)->receive_null (Simulator::now_us (), sources);
+	}
+}
---
+++ src/corba/parallel-channel-80211.h
@@ -0,0 +1,56 @@
+/* -*-	Mode:C++; c-basic-offset:8; tab-width:8; indent-tabs-mode:t -*- */
+/*
+ * Copyright (c) 2006 INRIA
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as 
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Mathieu Lacage, <mathieu.lacage@sophia.inria.fr>
+ */
+#ifndef PARALLEL_CHANNEL_80211_H
+#define PARALLEL_CHANNEL_80211_H
+
+#include <list>
+#include <stdint.h>
+#include "base-channel-80211.h"
+#include "context.h"
+
+class PropagationModel;
+class Packet;
+
+class ParallelChannel80211 : public yans::BaseChanell80211 {
+public:
+	ParallelChannel80211 ();
+	virtual ~ParallelChannel80211 ();
+	void add (Remote::Channel80211_var channel);
+	void send_null_message (void);
+	void receive (yans::Packet const *packet, double tx_power_dbm,
+		      double from_x, double from_y, double from_z,
+		      uint8_t tx_mode, uint8_t stuff);
+private:
+	virtual void real_add (yans::PropagationModel *model);
+	virtual void real_send (yans::Packet const *packet, double tx_power_dbm,
+				double from_x, double from_y, double from_z,
+				uint8_t tx_mode, uint8_t stuff, 
+				yans::PropagationModel const*caller) const;
+	typedef std::list<PropagationModel *> Models;
+	typedef std::list<PropagationModel *>::const_iterator ModelsCI;
+	Models m_models;
+	typedef std::list<Remote::Channel80211_var> Channels;
+	typedef std::list<Remote::Channel80211_var>::iterator ChannelsI;
+	Channels m_channels;
+};
+
+
+#endif /* PARALLEL_CHANNEL_80211_H */
