c61394d2f72ad5a749623629419093217159b256
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2006-06-28 11:25:21 +0200

    add Tag example

--- samples/main-packet.cc
+++ samples/main-packet.cc
@@ -2,10 +2,13 @@
 #include "yans/packet.h"
 #include "yans/chunk.h"
 #include "yans/buffer.h"
+#include "yans/tag.h"
 #include <iostream>
 
 using namespace yans;
 
+/* A sample Chunk implementation
+ */
 class MyChunk : public Chunk {
 public:
 	MyChunk ();
@@ -27,15 +30,19 @@ MyChunk::~MyChunk ()
 void 
 MyChunk::add_to (Buffer *buffer) const
 {
+	// reserve 2 bytes at head of buffer
 	buffer->add_at_start (2);
 	Buffer::Iterator i = buffer->begin ();
+	// serialize in head of buffer
 	i.write_hton_u16 (m_data);
 }
 void 
 MyChunk::remove_from (Buffer *buffer)
 {
 	Buffer::Iterator i = buffer->begin ();
+	// deserialize from head of buffer
 	m_data = i.read_ntoh_u16 ();
+	// remove deserialized data
 	buffer->remove_at_start (2);
 }
 void 
@@ -55,12 +62,55 @@ MyChunk::get_data (void) const
 	return m_data;
 }
 
+/* A sample Tag implementation
+ */
+class MyTag : public Tag {
+public:
+	MyTag ();
+	virtual ~MyTag ();
+	void set_stream_id (uint16_t stream_id);
+	uint16_t get_stream_id (void);
+private:
+	virtual uint32_t real_get_id (void) const;
+	virtual uint32_t real_get_size (void) const;
+	uint16_t m_stream_id;
+};
+
+MyTag::MyTag ()
+{}
+MyTag::~MyTag ()
+{}
+void 
+MyTag::set_stream_id (uint16_t stream_id)
+{
+	m_stream_id = stream_id;
+}
+uint16_t 
+MyTag::get_stream_id (void)
+{
+	return m_stream_id;
+}
+uint32_t 
+MyTag::real_get_id (void) const
+{
+	static uint32_t id = TagManager::register_tag ("MyTag");
+	return id;
+}
+uint32_t 
+MyTag::real_get_size (void) const
+{
+	return sizeof (*this);
+}
+
+
 static void
 receive (PacketPtr p)
 {
 	MyChunk my;
 	p->remove (&my);
 	std::cout << "received data=" << my.get_data () << std::endl;
+	MyTag my_tag;
+	p->remove_tag (&my_tag);
 }
 
 
@@ -71,6 +121,9 @@ int main (int argc, char *argv[])
 	my.set_data (2);
 	std::cout << "send data=2" << std::endl;
 	p->add (&my);
+	MyTag my_tag;
+	my_tag.set_stream_id (5);
+	p->add_tag (&my_tag);
 	receive (p);
 	return 0;
 }
