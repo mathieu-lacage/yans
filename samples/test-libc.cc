b7c48e4428675d58eead19bf41a22c116383df79
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2006-08-01 10:17:13 +0200

    real test case

---
+++ samples/test-libc.cc
@@ -0,0 +1,39 @@
+/* -*-	Mode:C++; c-basic-offset:8; tab-width:8; indent-tabs-mode:t -*- */
+/*
+ * Copyright (c) 2006 INRIA
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
+ */
+#include "yans/elf-process.h"
+#include "yans/simulator.h"
+
+using namespace yans;
+
+
+int main (int argc, char *argv[])
+{
+	ElfProcess a = ElfProcess ("a", "libtest-libc-binary.so");
+	ElfProcess b = ElfProcess ("b", "libtest-libc-binary.so");
+
+	a.add_arg ("a");
+	a.start ();
+	b.add_arg ("b");
+	b.start ();
+
+	Simulator::run ();
+	return 0;
+}
