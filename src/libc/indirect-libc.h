064966b78e8358a5200105e77da53dc97f0619e6
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2006-08-01 10:17:39 +0200

    forgot files

---
+++ src/libc/indirect-libc.h
@@ -0,0 +1,31 @@
+/* -*-	Mode:C; c-basic-offset:8; tab-width:8; indent-tabs-mode:t -*- */
+/*
+ * Copyright (c) 2006 INRIA
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
+ */
+#ifndef INDIRECT_LIBC_H
+#define INDIRECT_LIBC_H
+
+#include "bits/size-t.h"
+#include "bits/ssize-t.h"
+#include "bits/mode-t.h"
+#include "pthread.h"
+#include "libc.h"
+
+
+#endif /* INDIRECT_LIBC_H */
