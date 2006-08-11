41b849b2bd8cf0a1615c4678821d35057e808bd8
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2006-08-11 15:39:33 +0200

    re-implement tags to work with GPacket

--- src/common/tag.cc
+++ src/common/tag.cc
@@ -45,33 +45,84 @@ TagManager::instance (void)
 Tag::~Tag ()
 {}
 
+uint32_t Tags::m_prefered_size = 32;
+Tags::FreeList Tags::m_free_list;
 
-Tags::Tags ()
-	: m_real_size (32),
-	  m_size (0),
-	  m_data (new uint8_t [32])
-{}
-Tags::Tags (Tags const &o)
-	: m_real_size (o.m_real_size),
-	  m_size (o.m_size),
-	  m_data (new uint8_t [o.m_real_size])
+struct Tags::TagData *
+Tags::allocate (uint32_t size)
 {
-	memcpy (m_data, o.m_data, o.m_size);
-	m_index.insert (m_index.begin (), o.m_index.begin (), o.m_index.end ());
-}
-Tags::~Tags ()
-{
-	delete m_data;
-	m_index.erase (m_index.begin (), m_index.end ());
+	assert (size >= 1);
+	uint32_t buf_size = size + sizeof (struct TagData) - 1;
+	uint8_t *buf = new uint8_t [buf_size];
+	struct TagData *data = reinterpret_cast<struct TagData *> (buf);
+	data->m_size = size;
+	data->m_count = 1;
+	return data;
 }
 
 void
-Tags::copy_from (Tags const &o)
+Tags::deallocate (struct TagData *data)
 {
-	reset ();
-	reserve_at_end (o.m_size);
-	memcpy (m_data, o.m_data, o.m_size);
-	m_index.insert (m_index.begin (), o.m_index.begin (), o.m_index.end ());
+	uint8_t *buf = reinterpret_cast<uint8_t *> (data);
+	delete [] buf;
+}
+
+void
+Tags::recycle (struct TagData *data)
+{
+	if (data->m_size >= Tags::m_prefered_size) {
+		Tags::m_prefered_size = data->m_size;
+		Tags::m_free_list.push_back (data);
+	} else {
+		Tags::deallocate (data);
+	}
+}
+
+struct Tags::TagData *
+Tags::create (void)
+{
+	while (!Tags::m_free_list.empty ()) {
+		struct TagData *data = Tags::m_free_list.back ();
+		Tags::m_free_list.pop_back ();
+		if (data->m_size > Tags::m_prefered_size) {
+			data->m_count = 1;
+			return data;
+		}
+		Tags::deallocate (data);
+	}
+	struct TagData *data = Tags::allocate (Tags::m_prefered_size);
+	return data;
+}
+
+Tags::Tags ()
+	: m_size (0),
+	  m_data (Tags::create ())
+{}
+Tags::Tags (Tags const &o)
+	: m_size (o.m_size),
+	  m_data (o.m_data),
+	  m_index (o.m_index)
+{
+	m_data->m_count++;
+}
+Tags &
+Tags::operator = (Tags const &o)
+{
+	m_size = o.m_size;
+	m_data = o.m_data;
+	m_index = o.m_index;
+	m_data->m_count++;
+	return *this;
+}
+Tags::~Tags ()
+{
+	m_data->m_count--;
+	if (m_data->m_count == 0) {
+		Tags::recycle (m_data);
+	}
+#ifndef NDEBUG
+	m_index.erase (m_index.begin (), m_index.end ());
+#endif
 }
 
 void
@@ -84,12 +135,22 @@ Tags::reset (void)
 void
 Tags::reserve_at_end (uint32_t size)
 {
-	while (m_size + size > m_real_size) {
-		m_real_size *= 2;
-		uint8_t *data = new uint8_t [m_real_size];
-		delete m_data;
-		m_data = data;
+	if (m_size + size < m_data->m_size) {
+		return;
 	}
+	uint32_t new_size = m_data->m_size + size;
+	struct TagData *data = Tags::allocate (new_size);
+	memcpy (&data->m_data, &m_data->m_data, m_size);
+	m_data->m_count--;
+	if (m_data->m_count == 0) {
+		Tags::deallocate (m_data);
+	}
+}
+
+uint8_t *
+Tags::get_buffer (void) const
+{
+	return &m_data->m_data;
 }
 
 void 
@@ -101,7 +162,7 @@ Tags::add (Tag const *tag)
 	index.start = m_size;
 	m_index.push_back (index);
 	reserve_at_end (tag->real_get_size ());
-	memcpy (m_data+m_size, tag, tag->real_get_size ());
+	memcpy (get_buffer ()+m_size, tag, tag->real_get_size ());
 	m_size += tag->real_get_size ();
 }
 void 
@@ -110,7 +171,7 @@ Tags::peek (Tag *tag) const
 	uint32_t id = tag->real_get_id ();
 	for (IndexCI i = m_index.begin (); i != m_index.end (); i++) {
 		if (i->id == id) {
-			memcpy (tag, m_data + i->start, i->size);
+			memcpy (tag, get_buffer () + i->start, i->size);
 			assert (tag->real_get_id () == id);
 			return;
 		}
@@ -124,7 +185,7 @@ Tags::remove (Tag *tag)
 	uint32_t id = tag->real_get_id ();
 	for (IndexI i = m_index.begin (); i != m_index.end (); i++) {
 		if (i->id == id) {
-			memcpy (tag, m_data + i->start, i->size);
+			memcpy (tag, get_buffer () + i->start, i->size);
 			m_index.erase (i);
 			return;
 		}
@@ -138,7 +199,7 @@ Tags::update (Tag *tag)
 	uint32_t id = tag->real_get_id ();
 	for (IndexI i = m_index.begin (); i != m_index.end (); i++) {
 		if (i->id == id) {
-			memcpy (m_data + i->start, tag, i->size);
+			memcpy (get_buffer () + i->start, tag, i->size);
 			return;
 		}
 	}
